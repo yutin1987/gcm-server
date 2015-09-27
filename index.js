0f9dbf556678fce8d7f7ac228c40f179aed4accf
Yutin Liu (劉宇庭) <Yutin.Liu@mediatek.com>
2015-09-27 16:27:18 +0800

    redis

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+var bodyParser = require('body-parser')
 var express = require('express');
 var app = express();
+var redis = require("redis"),
+    client = redis.createClient()
 
-app.get('/', function (req, res) {
-  console.log(req);
-  res.send('Hello World!');
+app.use(bodyParser.json());
+
+app.post('/token', function (req, res) {
+  var deviceToken = req.body.deviceToken;
+  client.set(deviceToken, Date.now(), function (err, reply) {
+    console.log('save', deviceToken, err ? 'failed!' : 'ok!');
+  });
+  res.send('Hello Token!');
+});
+
+app.post('/psuh', function (req, res) {
+  var message = req.body.message;
+  console.log(message);
+  client.lpush('message', 'message', function (err, reply) {
+    console.log('push', message, err ? 'failed!' : 'ok!');
+  });
+  res.send('Hello Push!');
 });
 
 var server = app.listen(80, function () {
